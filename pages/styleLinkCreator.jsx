786b44cc5a4385008f470118e1442559cc33f350
Mahya Asadi <mahyaasadi378@gmail.com>
2024-02-18 22:28:23 +0330

    fix target bug in styleLinks

--- pages/styleLinkCreator.jsx
+++ pages/styleLinkCreator.jsx
@@ -77,7 +77,7 @@ const StyleLinkCreator = ({ UserData }) => {
         html = `<div class="row justify-center">${linkData
           .map(
             (link, index) =>
-              `<div class='col d-flex justify-center'><svg id="stroke" xmlns="http://www.w3.org/2000/svg" width="0" height="0"><defs><path id="line" d="M2 2c49.7 2.6 100 3.1 150 1.7-46.5 2-93 4.4-139.2 7.3 45.2-1.5 90.6-1.8 135.8-.6" fill="none" strokeLinecap="round" strokeLinejoin="round" vector-effect="non-scaling-stroke" /></defs></svg><a class="style3btn" href=${link.linkHref} target="_blank">${link.anchorText}<svg class="button-stroke" viewBox="0 0 154 13"><use href="#line"></use></svg><svg class="button-stroke" viewBox="0 0 154 13"><use href="#line"></use></svg></a></div>`
+              `<div class='col d-flex justify-center'><svg id="stroke" xmlns="http://www.w3.org/2000/svg" width="0" height="0"><defs><path id="line" d="M2 2c49.7 2.6 100 3.1 150 1.7-46.5 2-93 4.4-139.2 7.3 45.2-1.5 90.6-1.8 135.8-.6" fill="none" strokeLinecap="round" strokeLinejoin="round" vector-effect="non-scaling-stroke" /></defs></svg><a target="_blank" class="style3btn" href='${link.linkHref}' >${link.anchorText}<svg class="button-stroke" viewBox="0 0 154 13"><use href="#line"></use></svg><svg class="button-stroke" viewBox="0 0 154 13"><use href="#line"></use></svg></a></div>`
           )
           .join("")}</div>`;
       }
